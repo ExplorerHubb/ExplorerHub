00cd81f0bc6e9539940b8975b48a9140a8755935
seba ibraheem <sebaalmahdy@gmail.com>
2025-02-14 00:11:23 +0200

    updated the logged in account info

--- server/packages/urls.py
+++ server/packages/urls.py
@@ -8,7 +8,7 @@ router.register(r'Natural',views.NaturalPlacesView,basename='Natural_places')
 router.register(r'Beach',views.BeachPlacesView,basename='Beach_places')
 router.register(r'Religon',views.ReligonPlacesView,basename='Religon_places')
 router.register(r'Modern',views.ModernPlacesView,basename='Modern_places')
-router.register(r'Dessert',views.DessertPlacesView,basename='Dessert_places')
+router.register(r'Desert',views.DessertPlacesView,basename='Dessert_places')
 urlpatterns = [
     path('api/', include(router.urls)),
    
